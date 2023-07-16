36c6a89c5c42878c668afeaf1fd809f76eb99789
MUSHANA <mugumbyabenon03@gmail.com>
2023-07-16 20:56:05 +0300

    test

--- app.py
+++ app.py
@@ -1,7 +1,45 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+from cassandra.cluster import Cluster
+from cassandra.cluster import Cluster, ExecutionProfile, EXEC_PROFILE_DEFAULT
+from cassandra.policies import DCAwareRoundRobinPolicy, TokenAwarePolicy
+from flask_cqlalchemy import CQLAlchemy
+from cassandra.cqlengine import columns
+from datetime import datetime
+from cassandra.cqlengine.models import Model
+
+import uuid
 interns = []
 
+
+PATH_TO_BUNDLE_YAML = '/Users/mushana/Desktop/MY-GIT-CODE/Codeplay-Flask-Api/connect-bundle-CODEPALY UG.yaml'
+
+
+def get_cluster():
+    profile = ExecutionProfile(
+        load_balancing_policy=TokenAwarePolicy(
+            DCAwareRoundRobinPolicy(local_dc='us-east-1')
+        )
+    )
+
+    return Cluster(
+        execution_profiles={EXEC_PROFILE_DEFAULT: profile},
+        scylla_cloud=PATH_TO_BUNDLE_YAML,
+        )
+create_table_query = """
+    CREATE TABLE IF NOT EXISTS interns (
+        name TEXT,
+        email TEXT,
+        start_date DATE,
+        end_date DATE ,
+        PRIMARY KEY (email)
+    )
+    """
+cluster = get_cluster()
+session = cluster.connect() 
+session.set_keyspace('your_keyspace')
+   
 class Intern:
     def __init__(self, name, email, start_date, end_date):
         self.name = name
@@ -11,22 +49,43 @@ class Intern:
         
 
 app = Flask(__name__)
+if __name__ == '__main__':
+    app.run()
+
+
+
 
 CORS(app, origins=['*'], methods=['GET', 'POST'], allow_headers=['Content-Type'])
+
+
+@app.route('/')
+def index():
+  
+    return 'CodePlay API!'
 
 @app.route('/interns', methods=['POST'])
 def register_intern():
     data = request.get_json()
-    # Perform validation on the data
-    intern = Intern(data['name'], data['email'], data['start_date'], data['end_date'])
-    interns.append(intern)
+   
+    enter_data = "INSERT INTO interns (name, email, start_date, end_date) VALUES (%s, %s, %s, %s)"
+   
+    f = session.execute(enter_data, (data["name"], data["email"], data["start_date"], data["end_date"]))
+    print(f)
     return 'Intern registered successfully'
 
 
 
 @app.route('/interns', methods=['GET'])
 def get_interns():
-    return jsonify([vars(intern) for intern in interns])
+   
+    select_query = "SELECT name, email, start_date, end_date  FROM interns"
+    result = session.execute(select_query)
+    data = []
+    for row in result:
+        data.append({'name': row.name, 'email': row.email,
+        'start_date': str(row.start_date),
+            'end_date': str(row.end_date)})
+    return jsonify(data)  
 
 
 @app.route('/interns/<int:intern_id>', methods=['PUT', 'PATCH'])
---
+++ venv/lib/python3.11/site-packages/flask_cqlalchemy/__init__.py
@@ -0,0 +1,97 @@
+# -*- coding: utf-8 -*-
+"""
+flask_cqlalchemy
+
+:copyright: (c) 2015-2016 by George Thomas
+:license: BSD, see LICENSE for more details
+
+"""
+from cassandra.cqlengine import columns
+from cassandra.cqlengine import connection
+from cassandra.cqlengine import models
+from cassandra.cqlengine import usertype
+from cassandra.cqlengine.management import sync_table, create_keyspace_simple, sync_type
+
+
+class CQLAlchemy(object):
+    """The CQLAlchemy class. All CQLEngine methods are available as methods of
+    Model or columns attribute in this class.
+    No teardown method is available as connections are costly and once made are
+    ideally not disconnected.
+    """
+
+    def __init__(self, app=None):
+        """Constructor for the class"""
+        self._hosts_ = None
+        self._keyspace_ = None
+        self.columns = columns
+        self.Model = models.Model
+        self.UserType = usertype.UserType
+        self.app = app
+        self.sync_table = sync_table
+        self.sync_type = sync_type
+        self.create_keyspace_simple = create_keyspace_simple
+        if app is not None:
+            self.init_app(app)
+
+    def init_app(self, app):
+        """Bind the CQLAlchemy object to the app.
+
+        This method set all the config options for the connection to
+        the Cassandra cluster and creates a connection at startup.
+        """
+        self._hosts_ = app.config['CASSANDRA_HOSTS']
+        self._keyspace_ = app.config['CASSANDRA_KEYSPACE']
+        consistency = app.config.get('CASSANDRA_CONSISTENCY')  # by driver's default 'ConsistencyLevel.ONE' for all ops
+        lazy_connect = app.config.get('CASSANDRA_LAZY_CONNECT', False)
+        retry_connect = app.config.get('CASSANDRA_RETRY_CONNECT', False)
+        setup_kwargs = app.config.get('CASSANDRA_SETUP_KWARGS', {})
+
+        if not self._hosts_ and self._keyspace_:
+            raise NoConfig("""No Configuration options defined.
+            At least CASSANDRA_HOSTS and CASSANDRA_CONSISTENCY
+            must be supplied""")
+        connection.setup(self._hosts_,
+                         self._keyspace_,
+                         consistency=consistency,
+                         lazy_connect=lazy_connect,
+                         retry_connect=retry_connect,
+                         **setup_kwargs)
+
+    def sync_db(self):
+        """Sync all defined tables. All defined models must be imported before
+        this method is called
+        """
+        models = get_subclasses(self.Model)
+        for model in models:
+            sync_table(model)
+
+    def set_keyspace(self, keyspace_name=None):
+        """ Changes keyspace for the current session if keyspace_name is
+        supplied. Ideally sessions exist for the entire duration of the
+        application. So if the change in keyspace is meant to be temporary,
+        this method must be called again without any arguments
+        """
+        if not keyspace_name:
+            keyspace_name = self.app.config['CASSANDRA_KEYSPACE']
+        models.DEFAULT_KEYSPACE = keyspace_name
+        self._keyspace_ = keyspace_name
+
+
+class NoConfig(Exception):
+    """ Raised when CASSANDRA_HOSTS or CASSANDRA_KEYSPACE is not defined"""
+    pass
+
+
+# some helper functions for mashing the class list
+def flatten(lists):
+    """flatten a list of lists into a single list"""
+    return [item for sublist in lists for item in sublist]
+
+
+def get_subclasses(cls):
+    """get all the non abstract subclasses of cls"""
+    if cls.__abstract__:
+        return flatten([get_subclasses(scls) for scls in cls.__subclasses__()])
+    else:
+        return [cls]
